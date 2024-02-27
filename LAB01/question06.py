a9352c1c06f50934271abe7674a9e4501b547036
TheofilosDandakis <up1083718@ac.upatras.gr>
2024-02-28 00:35:00 +0200

    finale

--- LAB01/question06.py
+++ LAB01/question06.py
@@ -4,7 +4,7 @@ f=open("LAB01\weather.txt", mode="r", encoding="utf8")
 content=f.read()
 f.close()
 
-pattern=re.compile("\d+/\d+/\d+")
+pattern=re.compile("\S+ \d+/\d+/\d+")
 result=pattern.findall(content)
 
 print(result)
